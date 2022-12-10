559c0cec3e604101e46c10a069bc07e4debaad0b
vinecnttsai <105853560+vinecnttsai@users.noreply.github.com>
2022-12-10 16:39:49 +0800

    Update Soft_Body.cpp

--- Soft_Body.cpp
+++ Soft_Body.cpp
@@ -5,8 +5,24 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 using namespace sf;
-RenderWindow window(VideoMode(pixel+radius*2, pixel+radius*2), "Fluid Simulation");
+consst int pixel=1600,wid,height,particle_amount=wid*height;
+RenderWindow window(VideoMode(pixel, pixel), "Fluid Simulation");
 CircleShape circle;
+class particle
+{
+    vector<double> pos,F,mass,velocity;
+};
+particle p[particle_amount];
+void spring_mass_model()
+{
+    for(int i=0;i<particle_amount;i++)
+    {
+        if(!i||i==wid-1||i==particle_amount-1||i/wid==height-1)
+        else if(!i%(wid+1)||!i%(wid))
+        else if()
+        
+    }
+}
 int main()
 {
     circle.setRadius(radius);
